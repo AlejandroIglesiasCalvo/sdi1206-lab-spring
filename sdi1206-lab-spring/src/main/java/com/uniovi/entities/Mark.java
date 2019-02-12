420af8844b67dfc661315df65116509798017aa8
Alejandro Iglesias Calvo <uo24346@uniovi.es>
2019-02-12 14:45:17 +0100

    Lo dejo en el punto 4.4, que se acaba la clase

--- sdi1206-lab-spring/src/main/java/com/uniovi/entities/Mark.java
+++ sdi1206-lab-spring/src/main/java/com/uniovi/entities/Mark.java
@@ -1,4 +1,5 @@
 package com.uniovi.entities;
+
 import javax.persistence.*;
 
 @Entity
@@ -9,11 +10,22 @@ public class Mark {
 	private String description;
 	private Double score;
 
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
 	public Mark(Long id, String description, Double score) {
 		super();
 		this.id = id;
 		this.description = description;
 		this.score = score;
+	}
+
+	public Mark(String description, Double score, User user) {
+		super();
+		this.description = description;
+		this.score = score;
+		this.user = user;
 	}
 
 	public Mark() {
@@ -41,6 +53,13 @@ public class Mark {
 
 	public void setScore(Double score) {
 		this.score = score;
+	}
+
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user ) {
+		this.user = user;
 	}
 
 	// Lo pongo abajo porque estoy acostumbrado a que este aqui
