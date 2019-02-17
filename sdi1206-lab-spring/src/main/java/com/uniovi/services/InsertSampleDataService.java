392991bce1cf353d9c21a1ef731db56d563982de
Alejandro Iglesias Calvo <uo24346@uniovi.es>
2019-02-17 21:00:22 +0100

    SDI-SpringBoot- 5 - Autenticación y control de acceso
    Problemas ya reportados al profesor en un email.

--- sdi1206-lab-spring/src/main/java/com/uniovi/services/InsertSampleDataService.java
+++ sdi1206-lab-spring/src/main/java/com/uniovi/services/InsertSampleDataService.java
@@ -1,4 +1,5 @@
 package com.uniovi.services;
+
 import java.util.HashSet;
 import java.util.Set;
 import javax.annotation.PostConstruct;
@@ -6,20 +7,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.uniovi.entities.Mark;
 import com.uniovi.entities.User;
+
 @Service
 public class InsertSampleDataService {
-	
-	//Si eliminamos la etiqueta service no hace falta que la base se resetee todas las veces
+
+	// Si eliminamos la etiqueta service no hace falta que la base se resetee todas
+	// las veces
 	@Autowired
 	private UsersService usersService;
+
 	@PostConstruct
 	public void init() {
 		User user1 = new User("99999990A", "Pedro", "Díaz");
+		user1.setPassword("123456");
 		User user2 = new User("99999991B", "Lucas", "Núñez");
+		user2.setPassword("123456");
 		User user3 = new User("99999992C", "María", "Rodríguez");
+		user3.setPassword("123456");
 		User user4 = new User("99999993D", "Marta", "Almonte");
+		user4.setPassword("123456");
 		User user5 = new User("99999977E", "Pelayo", "Valdes");
+		user5.setPassword("123456");
 		User user6 = new User("99999988F", "Edward", "Núñez");
+		user6.setPassword("123456");
 		Set user1Marks = new HashSet<Mark>() {
 			{
 				add(new Mark("Nota A1", 10.0, user1));
