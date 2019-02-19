e06ffb23580a8c8f2fd031d9ba93ab559d1ff205
Alejandro Iglesias Calvo <uo24346@uniovi.es>
2019-02-19 14:23:06 +0100

    SDI-SpringBoot-Sesión-4.2-Roles

--- sdi1206-lab-spring/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi1206-lab-spring/src/main/java/com/uniovi/controllers/UsersController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.uniovi.entities.*;
+import com.uniovi.services.RolesService;
 import com.uniovi.services.SecurityService;
 import com.uniovi.services.UsersService;
 import com.uniovi.validator.SignUpFormValidator;
@@ -24,6 +25,9 @@ public class UsersController {
 	@Autowired
 	private SignUpFormValidator signUpFormValidator;
 
+	@Autowired
+	private RolesService rolesService;
+
 	@RequestMapping("/user/list")
 	public String getListado(Model model) {
 		model.addAttribute("usersList", usersService.getUsers());
@@ -32,7 +36,7 @@ public class UsersController {
 
 	@RequestMapping(value = "/user/add")
 	public String getUser(Model model) {
-		model.addAttribute("usersList", usersService.getUsers());
+		model.addAttribute("rolesList", rolesService.getRoles());
 		return "user/add";
 	}
 
@@ -70,24 +74,22 @@ public class UsersController {
 
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signup(Model model) {
-	model.addAttribute("user", new User());
-	 return "signup";
+		model.addAttribute("user", new User());
+		return "signup";
 	}
 
-
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public String signup(@Validated User user, BindingResult result, Model
+	public String signup(@ModelAttribute @Validated User user, BindingResult result, Model
 			model) {
 		signUpFormValidator.validate(user, result);
 		if (result.hasErrors()) {
 			return "signup";
 		}
+		user.setRole(rolesService.getRoles()[0]);
 		usersService.addUser(user);
 		securityService.autoLogin(user.getDni(), user.getPasswordConfirm());
 		return "redirect:home";
 	}
-
-
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(Model model) {
